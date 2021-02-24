b05fe3562d9f83fe6f668c4cbbd1afc46c0eb4d3
Deibson Cogo <deibsoncogo@outlook.com>
2021-02-23 21:39:21 -0300

    Botões teste

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+import "./styles/global.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello NLW</h1>
+      <Button color="red">Primeiro</Button>
+      <Button color="blue">Segundo</Button>
+      <Button color="green">Terceiro</Button>
+    </div>
+  );
+}
+
+export default App;
